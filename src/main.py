9ca25e8473b17425d0b94cdbda0c369398c7b6c9
Bahna-Darius <emanuelbahna@gmail.com>
2025-04-24 19:19:56 +0300

    Main control Architecture

--- src/main.py
+++ src/main.py
@@ -1,23 +1,36 @@
-# Import Libraries:
+from src.model import detect, model
+from src.control import move_to
 import cv2
 import os
 
 
-# Open Camera:
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-ret, frame = cap.read()
-cap.release()
-frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+# Paths:
+os.makedirs('../data/input', exist_ok=True)
+os.makedirs('../data/output', exist_ok=True)
+input_path = 'data/input/predict_test.jpg'
+output_path = 'data/output/predict_test.jpg'
 
-# Save Picture:
-filename = 'frame_test.jpg'
-folder = '../data/input'
 
-os.makedirs(folder, exist_ok=True)
-path = os.path.join(folder, filename)
-ok = cv2.imwrite(path, frame)
+def main():
+    capture_picture = cv2.VideoCapture(0)
 
-if ok:
-        print(f"✅ Imagine salvată în {path}")
-else:
-    print("❌ Eroare la salvarea imaginii")
+    for _ in range(10):
+        ret, frame = capture_picture.read()
+    capture_picture.release()
+    if not ret:
+        raise RuntimeError("Could not read the frame from the Pi camera")
+
+    cv2.imwrite(input_path, frame)
+    # Predict image:
+    label = detect(frame)
+    print("\n🔖 Predicted:", label)
+
+    # Move servo according to label:
+    move_to(label)
+
+    # Save predicted picture:
+    annotated = model(frame[..., ::-1]).render()[0]
+    cv2.imwrite(output_path, annotated)
+
+if __name__ == "__main__":
+    main()
